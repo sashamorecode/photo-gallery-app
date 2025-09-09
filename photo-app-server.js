b9aea5b413e2addd05ebb9ffeb82c75fb4e613c5
Sasha Salzano-Weir <sashasalzweir@gmail.com>
2025-09-09 18:42:08 +0200

    change port

--- photo-app-server.js
+++ photo-app-server.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 import https from 'https';
 import { handler } from './build/handler.js';
-
+import env from '$app/environment'
 const cert = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/fullchain.pem');
 const key = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/privkey.pem');
 
 https.createServer({ key, cert }, handler)
-  .listen(443, () => {
+  .listen(300, () => {
     console.log('HTTPS server running on port 443');
   });
 
