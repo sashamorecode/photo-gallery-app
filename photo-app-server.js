b61118fc82be62fee4d776da9fe6c9222b19e2f4
Sasha Salzano-Weir <sashasalzweir@gmail.com>
2025-09-09 18:37:34 +0200

    add cusotme node server

---
+++ photo-app-server.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import https from 'https';
+import { handler } from './build/handler.js';
+
+const cert = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/fullchain.pem');
+const key = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/privkey.pem');
+
+https.createServer({ key, cert }, handler)
+  .listen(443, () => {
+    console.log('HTTPS server running on port 443');
+  });
+
