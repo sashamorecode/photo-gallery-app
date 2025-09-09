060a5535fc860d7653c215c62e8cdf2c8e767172
Sasha Salzano-Weir <sashasalzweir@gmail.com>
2025-09-09 18:48:22 +0200

    G

--- photo-app-server.js
+++ photo-app-server.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import https from 'https';
 import { handler } from './build/handler.js';
-import env from '$app/environment'
 const cert = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/fullchain.pem');
 const key = fs.readFileSync('/etc/letsencrypt/live/jonasschledorn.com/privkey.pem');
 
