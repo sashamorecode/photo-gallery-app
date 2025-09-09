75f153af7b2abdaad01b8eb92952f1104eeee6e0
Sasha Salzano-Weir <sashasalzweir@gmail.com>
2025-09-09 17:44:56 +0200

    add cert

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
-    plugins: [basicSsl(), tailwindcss(), sveltekit()]
+    plugins: [basicSsl(
+        {
+            name:"jonasschledorn.com",
+            domains:["*.jonasschledorn.com"],
+            certDir:"./certs"
+        }
+    ), tailwindcss(), sveltekit()]
 });
