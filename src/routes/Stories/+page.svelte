fa91427ceeda119af9fcda9ceebf195c6423cc68
Sasha Salzano-Weir <sashasalzweir@gmail.com>
2026-01-28 18:45:22 +0100

    change stories ui

--- src/routes/Stories/+page.svelte
+++ src/routes/Stories/+page.svelte
@@ -1,41 +1,36 @@
 <script>
-    import Navbar from "$lib/Navbar.svelte";
-    let { data } = $props();
-    let storys = data.stories;
-    console.log(storys);
+  import Navbar from "$lib/Navbar.svelte";
+  let { data } = $props();
+  let storys = data.stories;
+  console.log(storys);
 </script>
-
 
 <Navbar />
 <div class="w-full h-full overflow-y-auto">
-    <h1
-        class="text-4xl font-cabin font-[400] pl-12 pt-3 absolute w-full bg-black pb-4 lg:hidden"
-    >
-        Stories
-    </h1>
-    <div class="pt-20 lg:pt-20 mx-auto grid grid-cols-1 gap-6 p-4 lg:pl-0">
-        <!-- Story Items -->
-        {#each storys as { coverImage, title, url, alt }}
-            <a href="/Stories/{url}" data-sveltekit-preload-data>
-                <div
-                    class="block story-item mx-auto"
-                    data-story="1"
-                >
-                    <div class="relative w-full lg:w-3/4 cursor-pointer group lg:mx-auto">
-                        <img
-                            src={coverImage}
-                            {alt}
-                            class="w-full object-cover rounded-lg transition-transform lg:group-hover:scale-[101%] mx-center"
-                        />
-                        <h1
-                            class="transition-transform absolute top-1/2 left-1/2 -translate-x-1/4 -translate-y-1/4
-                                   text-4xl group-hover:bg-black/100 bg-black/70 px-2 pt-2 rounded-lg"
-                        >
-                            {title}
-                        </h1>
-                    </div>
-                </div>
-            </a>
-        {/each}
-    </div>
+  <h1
+    class="text-4xl font-cabin font-[400] pl-12 pt-3 absolute w-full bg-black pb-4 lg:hidden"
+  >
+    Stories
+  </h1>
+  <div class="pt-20 lg:pt-20 mx-auto grid grid-cols-1 gap-6 p-4 lg:pl-0">
+    <!-- Story Items -->
+    {#each storys as { coverImage, title, url, alt }}
+      <a href="/Stories/{url}" data-sveltekit-preload-data>
+        <div class="block story-item mx-auto" data-story="1">
+          <div class="relative w-full lg:w-3/4 cursor-pointer group lg:mx-auto">
+            <img
+              src={coverImage}
+              {alt}
+              class="w-full object-cover rounded-lg transition-transform lg:group-hover:scale-[101%] mx-center"
+            />
+            <h1
+              class="text-center transition-transform text-3xl group-hover:bg-black/100 bg-black/70 px-2 pt-2 rounded-lg"
+            >
+              {title}
+            </h1>
+          </div>
+        </div>
+      </a>
+    {/each}
+  </div>
 </div>
